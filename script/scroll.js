74221fbf6e4509aa70ed67469b9db4e8361c02d5
bgheller <102472419+bgheller@users.noreply.github.com>
2022-04-07 20:59:29 +0300

    Add files via upload

---
+++ script/scroll.js
@@ -0,0 +1,283 @@
+const element = document.querySelector('.in__line');
+const check = document.querySelector('.animation');
+const observer = new IntersectionObserver(entries => { 
+  if (!element.classList.contains('animation')) {
+    element.classList.toggle( 'animation', entries[0].isIntersecting );
+  }
+  
+});
+
+observer.observe( element );
+
+//--------------------------------------------------------------------------
+
+const element1 = document.querySelector('.hidden');
+const check1 = document.querySelector('.animation1');
+const observer1 = new IntersectionObserver(entries => { 
+  if (!element1.classList.contains('animation1')) {
+    element1.classList.toggle( 'animation1', entries[0].isIntersecting );
+  }
+  
+});
+
+observer1.observe( element1 );
+
+//--------------------------------------------------------------------------
+
+const element2 = document.querySelector('#first');
+const check2 = document.querySelector('.animation2');
+const observer2 = new IntersectionObserver(entries => { 
+  if (!element2.classList.contains('animation2')) {
+    element2.classList.toggle( 'animation2', entries[0].isIntersecting );
+  }
+  
+});
+
+observer2.observe( element2 );
+
+//--------------------------------------------------------------------------
+
+const element3 = document.querySelector('#second');
+const check3 = document.querySelector('.animation3');
+const observer3 = new IntersectionObserver(entries => { 
+  if (!element3.classList.contains('animation3')) {
+    element3.classList.toggle( 'animation3', entries[0].isIntersecting );
+  }
+  
+});
+
+observer3.observe( element3 );
+
+//--------------------------------------------------------------------------
+
+const element4 = document.querySelector('#third');
+const check4 = document.querySelector('.animation4');
+const observer4 = new IntersectionObserver(entries => { 
+  if (!element4.classList.contains('animation4')) {
+    element4.classList.toggle( 'animation4', entries[0].isIntersecting );
+  }
+  
+});
+
+observer4.observe( element4 );
+
+//--------------------------------------------------------------------------
+
+const element5 = document.querySelector('#fourth');
+const check5 = document.querySelector('.animation5');
+const observer5 = new IntersectionObserver(entries => { 
+  if (!element5.classList.contains('animation5')) {
+    element5.classList.toggle( 'animation5', entries[0].isIntersecting );
+  }
+  
+});
+
+observer5.observe( element5 );
+
+//--------------------------------------------------------------------------
+
+const element6 = document.querySelector('#fifth');
+const check6 = document.querySelector('.animation6');
+const observer6 = new IntersectionObserver(entries => { 
+  if (!element6.classList.contains('animation6')) {
+    element6.classList.toggle( 'animation6', entries[0].isIntersecting );
+  }
+  
+});
+
+observer6.observe( element6 );
+
+//--------------------------------------------------------------------------
+
+const element7 = document.querySelector('#sixth');
+const check7 = document.querySelector('.animation7');
+const observer7 = new IntersectionObserver(entries => { 
+  if (!element7.classList.contains('animation7')) {
+    element7.classList.toggle( 'animation7', entries[0].isIntersecting );
+  }
+  
+});
+
+observer7.observe( element7 );
+
+//--------------------------------------------------------------------------
+
+const element8 = document.querySelector('#a1');
+const check8 = document.querySelector('.animation8');
+const observer8 = new IntersectionObserver(entries => { 
+  if (!element8.classList.contains('animation8')) {
+    element8.classList.toggle( 'animation8', entries[0].isIntersecting );
+  }
+  
+});
+
+observer8.observe( element8 );
+
+//--------------------------------------------------------------------------
+
+const element9 = document.querySelector('#a2');
+const check9 = document.querySelector('.animation9');
+const observer9 = new IntersectionObserver(entries => { 
+  if (!element9.classList.contains('animation9')) {
+    element9.classList.toggle( 'animation9', entries[0].isIntersecting );
+  }
+  
+});
+
+observer9.observe( element9 );
+
+//--------------------------------------------------------------------------
+
+const element10 = document.querySelector('#a3');
+const check10 = document.querySelector('.animation10');
+const observer10 = new IntersectionObserver(entries => { 
+  if (!element10.classList.contains('animation10')) {
+    element10.classList.toggle( 'animation10', entries[0].isIntersecting );
+  }
+  
+});
+
+observer10.observe( element10 );
+
+//--------------------------------------------------------------------------
+
+const element11 = document.querySelector('#a4');
+const check11 = document.querySelector('.animation11');
+const observer11 = new IntersectionObserver(entries => { 
+  if (!element11.classList.contains('animation11')) {
+    element11.classList.toggle( 'animation11', entries[0].isIntersecting );
+  }
+  
+});
+
+observer11.observe( element11 );
+
+//--------------------------------------------------------------------------
+
+const element12 = document.querySelector('#a5');
+const check12 = document.querySelector('.animation12');
+const observer12 = new IntersectionObserver(entries => { 
+  if (!element12.classList.contains('animation12')) {
+    element12.classList.toggle( 'animation12', entries[0].isIntersecting );
+  }
+  
+});
+
+observer12.observe( element12 );
+
+//--------------------------------------------------------------------------
+
+const element13 = document.querySelector('#a6');
+const check13 = document.querySelector('.animation13');
+const observer13 = new IntersectionObserver(entries => { 
+  if (!element13.classList.contains('animation13')) {
+    element13.classList.toggle( 'animation13', entries[0].isIntersecting );
+  }
+  
+});
+
+observer13.observe( element13 );
+
+//--------------------------------------------------------------------------
+
+const element14 = document.querySelector('#a');
+const check14 = document.querySelector('.animation14');
+const observer14 = new IntersectionObserver(entries => { 
+  if (!element14.classList.contains('animation14')) {
+    element14.classList.toggle( 'animation14', entries[0].isIntersecting );
+  }
+  
+});
+
+observer14.observe( element14 );
+
+//--------------------------------------------------------------------------
+
+const element15 = document.querySelector('#b');
+const check15 = document.querySelector('.animation15');
+const observer15 = new IntersectionObserver(entries => { 
+  if (!element15.classList.contains('animation15')) {
+    element15.classList.toggle( 'animation15', entries[0].isIntersecting );
+  }
+  
+});
+
+observer15.observe( element15 );
+
+//--------------------------------------------------------------------------
+
+const element16 = document.querySelector('#c');
+const check16 = document.querySelector('.animation16');
+const observer16 = new IntersectionObserver(entries => { 
+  if (!element16.classList.contains('animation16')) {
+    element16.classList.toggle( 'animation16', entries[0].isIntersecting );
+  }
+  
+});
+
+observer16.observe( element16 );
+
+//--------------------------------------------------------------------------
+
+const element17 = document.querySelector('#d');
+const check17 = document.querySelector('.animation17');
+const observer17 = new IntersectionObserver(entries => { 
+  if (!element17.classList.contains('animation17')) {
+    element17.classList.toggle( 'animation17', entries[0].isIntersecting );
+  }
+  
+});
+
+observer17.observe( element17 );
+
+//--------------------------------------------------------------------------
+
+const element18 = document.querySelector('#e');
+const check18 = document.querySelector('.animation18');
+const observer18 = new IntersectionObserver(entries => { 
+  if (!element18.classList.contains('animation18')) {
+    element18.classList.toggle( 'animation18', entries[0].isIntersecting );
+  }
+  
+});
+
+observer18.observe( element18 );
+
+//--------------------------------------------------------------------------
+
+const element19 = document.querySelector('#f');
+const check19 = document.querySelector('.animation19');
+const observer19 = new IntersectionObserver(entries => { 
+  if (!element19.classList.contains('animation19')) {
+    element19.classList.toggle( 'animation19', entries[0].isIntersecting );
+  }
+  
+});
+
+observer19.observe( element19 );
+
+//--------------------------------------------------------------------------
+
+const element20 = document.querySelector('#text3');
+const check20 = document.querySelector('.animation20');
+const observer20 = new IntersectionObserver(entries => { 
+  if (!element20.classList.contains('animation20')) {
+    element20.classList.toggle( 'animation20', entries[0].isIntersecting );
+  }
+  
+});
+
+observer20.observe( element20 );
+
+//--------------------------------------------------------------------------
+
+const element21 = document.querySelector('.pizza');
+const check21 = document.querySelector('.animation21');
+const observer21 = new IntersectionObserver(entries => { 
+  if (!element21.classList.contains('animation21')) {
+    element21.classList.toggle( 'animation21', entries[0].isIntersecting );
+  }
+  
+});
+
+observer21.observe( element21 );
